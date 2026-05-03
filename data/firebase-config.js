f0124f0dc17bb6b8e9696b742f3e7d4c019fbbd6
kigalifreshcart-cmd <kigalifreshcart@gmail.com>
2026-05-03 15:25:51 +0200

    update

--- data/firebase-config.js
+++ data/firebase-config.js
@@ -7,12 +7,6 @@
   4) Deploy this file with the website.
 */
 window.FRESHCART_FIREBASE = {
-  apiKey: "AIzaSyAK-9tFVVNnVL80r3WrEYadmFdX3VMWVMI",
-  authDomain: "kigali-fresh-cart.firebaseapp.com",
-  projectId: "kigali-fresh-cart",
-  storageBucket: "kigali-fresh-cart.firebasestorage.app",
-  messagingSenderId: "919046854659",
-  appId: "1:919046854659:web:a47f93bca7b7e2d3d5d636",
-  measurementId: "G-XZEXZ907BV"
+  databaseURL: "",
   path: "freshcart/config"
 };
